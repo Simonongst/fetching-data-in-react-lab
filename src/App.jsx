7fe6b2290a5c7bd9f7dab84b9d4766cc75a8d4fc
Simon Ong <simonongst@gmail.com>
2025-09-12 23:32:30 +0800

    feat: fetch starships on load and pass displayedStarships to StarshipList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import './App.css'
+import * as starshipService from './services/starshipService'
+import StarshipSearch from './components/StarshipSearch/StarshipSearch'
+import StarshipList from './components/StarshipList/StarshipList';
+import {useState, useEffect} from 'react'
 
 const App = () => {
+  const [starshipsData, setStarshipsData] = useState([]);
+  const [displayedStarships, setDisplayedStarships] = useState([]);
+
+  useEffect(() => {
+    const fetchStarships = async () => {
+      try {
+        const data = await starshipService.index();
+        setStarshipsData(data);
+        setDisplayedStarships(data);
+      } catch (error) {
+        console.error('Failed to load starships:', error);
+      }
+    };
+    fetchStarships();
+  }, [])
 
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <StarshipSearch />
+      <StarshipList starships={displayedStarships}/>
+    </div>
   );
 }
 
---
+++ src/components/StarshipList/StarshipList.jsx
@@ -0,0 +1,11 @@
+import StarshipCard from '../StarshipCard/StarshipCard'
+
+const StarshipList = () => {
+  return (
+    <div>
+        <StarshipCard />
+    </div>
+  )
+}
+
+export default StarshipList
